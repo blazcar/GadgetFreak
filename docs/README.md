8eb4e85263d7ec04d92bd9255fae34ba124583bc
Blaž Čar <Bla ar>
2016-11-27 23:22:59 +0100

    Zadnji commit

--- docs/README.md
+++ docs/README.md
@@ -15,3 +15,10 @@ CILJNA PUBLIKA
 
 To aplikacijo bodo uporabljali navdušenci nad mobilnimi napravami in dotatki za slednje. Ker bodo registrirani uporabniki imeli možnost dodajanja novih tem, objav in naprav naj bi imeli vsaj malo znanja o področju, o katerem teče tema na tej spletni strani.
 
+DELOVANJE V BRSKALNIKIH
+
+Delovanje sem stestiral v brskalniku Chrome ter Firefox in deluje brezhibno
+
+POTREBNO JE REŠITI ŠE:
+Stran je treba dodelati z implementacijo "backenda" saj menim, da brez tega nikakor ne more delovati tako, kot je bilo mišljeno, saj vsi komentarji, specifikacije in forumi zahtevajo veliko izvajanja v ozadju.
+
--- script/regUser.js
+++ script/regUser.js
@@ -1,3 +1,5 @@
+
+
 function dodajTemo() {
 	var ul = document.getElementById("teme_foruma");
 	  var li = document.createElement("li");
@@ -7,3 +9,20 @@ function dodajTemo() {
 	  li.appendChild(a);
 	  ul.appendChild(li);
 }
+
+
+function poslji_novo_objavo(){
+	var string = "<div class = \"post samsung\">\
+              <div class = \"tekst_objave\">\
+                <a class = \"wholePost\" href=\"lookPost.html\">More</a>\
+                <p class = \"Title\">" + document.getElementById("naslov_objave").value+"</p>\
+                <p>" + document.getElementById("teskt_nove_objave").value + "</p>\
+                <textarea name=\"comment\" required placeholder=\"Vaš komentar\"></textarea>\
+                <input class=\"button_comment\" type=\"submit\" value=\"Komentiraj!\"/>\
+              </div>\
+          </div>";
+
+	document.getElementById("popular_posts_regUser").innerHTML += string;
+
+
+}	
